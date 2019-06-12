506f10aa0e86824a4d98f038222f7e9412f8cc8e
Deivu <davexdme@gmail.com>
2019-06-12 17:05:52 +0800

    Feat. Improved Code + Added Documentation

--- README.md
+++ README.md
@@ -1,16 +1,31 @@
 # Zuikaku
 <p align="center">
-  <img src="https://vignette.wikia.nocookie.net/kancolle/images/c/ce/Zuikaku_Christmas_Full.png/revision/latest">
+  <img src="https://vignette.wikia.nocookie.net/kancolle/images/8/84/Zuikaku_Christmas_Full_Damaged.png/revision/latest/">
 </p>
+The ShipGirl Project. Zuikaku. `(c) Kancolle for Zuikaku`.
 
 ### A very lightweight wrapper around osu! api.
 
-### Usage.
-Refer to [`Client.js`](https://github.com/Deivu/Zuikaku/blob/master/test/Client.js) 
+### Documentation
 
-### This API mostly follow the parameters found in osu! api wiki with the exception of you dont need to provide the api key in every request like how it is presented on example.
+### Example Usage
+```js
+const { Zuikaku } = require('../index.js');
+const zuikaku = new Zuikaku('Your_Token');
+async function start() {
+  const user = await zuikaku.getUser({ u: 'Deivu', m: 0 });
+  console.log(user[0]);
+  const userBest = await zuikaku.getUserBest({ u: 'Deivu', m: 0 });
+  console.log(userBest[0]);
+  const beatmap = await zuikaku.getBeatmaps({ b: 1619555, m: 0 });
+  console.log(beatmap[0]);
+}
+start()
+  .then(() => console.log('All requests are done'))
+  .catch(console.error);
+```
 
+Or this [`Client.js`](https://github.com/Deivu/Zuikaku/blob/master/test/Client.js) 
+
+### osu! API wiki
 You can find the wiki in [`HERE`](https://github.com/ppy/osu-api/wiki) 
-
-### NOTE
-> This is in development but stable enough to be on production and I would add some minor features in future.
--- src/Zuikaku.js
+++ src/Zuikaku.js
@@ -2,59 +2,108 @@ const Fetch = require('node-fetch');
 const Abort = require('abort-controller');
 
 class Zuikaku {
-    constructor(token) {
-        Object.defineProperty(this, 'token', { value: token });
-        this.baseurl = 'https://osu.ppy.sh/api';
+    /**
+     * ZuikakuOptions
+     * @typedef {Object} ZuikakuOptions
+     * @memberof Zuikaku
+     * @property {string} token API key from osu! api.
+     * @property {number} [timeout=5000] Timeout before a request times out.
+     */
+
+    /**
+     * Zuikaku, osu! API Wrappper
+     * @param {ZuikakuOptions} ZuikakuOptions ZuikakuOptions
+     */
+    constructor(options) {
+        if (!options.token)
+            throw new Error('Zuikaku_Error: Token not specified');
+        Object.defineProperty(this, 'token', { value: options.token });
+        Object.defineProperty(this, 'timeout', { value: options.timeout || 5000 });
+        Object.defineProperty(this, 'baseurl', { value: 'https://osu.ppy.sh/api' });
     }
 
-    // Beatmap
+    /**
+     * Retrieves info about a beatmap
+     * @param {Object} parameters Query Parameters that follow the osu! api wiki format. https://github.com/ppy/osu-api/wiki#beatmap
+     * @returns {Promise<Array>} A JSON list containing all beatmaps (one per difficulty) matching criteria.
+     */
     getBeatmaps(parameters) {
         if (!parameters) throw new Error('Zuikaku_Error: Parameters is not specified');
         return this._fetch('/get_beatmaps', parameters);
     }
 
-    // User
+    /**
+     * Retrieves info about a user
+     * @param {Object} parameters Query Parameters that follow the osu! api wiki format. https://github.com/ppy/osu-api/wiki#apiget_user
+     * @returns {Promise<Array>} A JSON list containing user information.
+     */
     getUser(parameters) {
         if (!parameters) throw new Error('Zuikaku_Error: Parameters is not specified');
         return this._fetch('/get_user', parameters);
     }
 
-    // Scores
+    /**
+     * Retrieves the top 100 scores from a specific beatmap.
+     * @param {Object} parameters Query Parameters that follow the osu! api wiki format. https://github.com/ppy/osu-api/wiki#apiget_scores
+     * @returns {Promise<Array>} A JSON list containing the top 100 scores of the specified beatmap.
+     */
     getScores(parameters) {
         if (!parameters) throw new Error('Zuikaku_Error: Parameters is not specified');
         return this._fetch('/get_scores', parameters);
     }
 
-    // Best Performance
+    /**
+     * Gets the top scores from a specific user
+     * @param {Object} parameters Query Parameters that follow the osu! api wiki format. https://github.com/ppy/osu-api/wiki#apiget_user_best
+     * @returns {Promise<Array>} A JSON list containing the top 10 scores for the specified user.
+     */
     getUserBest(parameters) {
         if (!parameters) throw new Error('Zuikaku_Error: Parameters is not specified');
         return this._fetch('/get_user_best', parameters);
     }
 
-    // Recently Played
+    /**
+     * Gets the user's ten most recent plays over the last 24 hours.
+     * @param {Object} parameters Query Parameters that follow the osu! api wiki format. https://github.com/ppy/osu-api/wiki#apiget_user_recent
+     * @returns {Promise<Array>} A JSON list containing the user's ten most recent songs played.
+     */
     getUserRecent(parameters) {
         if (!parameters) throw new Error('Zuikaku_Error: Parameters is not specified');
         return this._fetch('/get_user_recent', parameters);
     }
 
-    // Multiplayer
+    /**
+     * Retrieve information about multiplayer match.
+     * @param {Object} parameters Query Parameters that follow the osu! api wiki format. https://github.com/ppy/osu-api/wiki#apiget_match
+     * @returns {Promise<JSON>} A JSON object containing match information, and player's result.
+     */
     getMatch(parameters) {
         if (!parameters) throw new Error('Zuikaku_Error: Parameters is not specified');
         return this._fetch('/get_match', parameters);
     }
 
-    // Replay Data
+    /**
+     * Get the replay data of a user's score on a map.
+     * @param {Object} parameters Query Parameters that follow the osu! api wiki format. https://github.com/ppy/osu-api/wiki#get-replay-data
+     * @returns {Promise<JSON>} A JSON object containing the key "content", which is a base64-encoded replay.
+     */
     getReplay(parameters) {
         if (!parameters) throw new Error('Zuikaku_Error: Parameters is not specified');
         return this._fetch('/get_replay', parameters);
     }
-
+    /**
+     * Get the replay data of a user's score on a map.
+     * @param {string} endpoint the endpoint to use.
+     * @param {Object} options the options for that endpoint.
+     * @returns {Promise<*>} The response of the request
+     * @private
+     */
     _fetch(endpoint, options) {
         const url = new URL(this.baseurl + endpoint);
         options.k = this.token;
         url.search = new URLSearchParams(options);
         const controller = new Abort();
-        const timeout = setTimeout(() => controller.abort(), 15000);
+        const timeout = setTimeout(() => controller.abort(), this.timeout);
         return Fetch(url.toString(), { signal: controller.signal })
             .then((response) => {
                 clearTimeout(timeout);
